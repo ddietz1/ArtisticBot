666f444e2ce52d431002a91e2b47a7fad6acd373
gaiosa <gregory.aiosa@gmail.com>
2026-05-30 20:40:44 -0500

    Stop tracking ROS 2 build, install, and log directories

--- ws/interbotix/build/interbotix_common_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derek-dietz/MSAI/ComputerVision/Final_Project/ws/interbotix/src/interbotix_ros_toolboxes/interbotix_common_toolbox/interbotix_common_sim
-# Build directory: /home/derek-dietz/MSAI/ComputerVision/Final_Project/ws/interbotix/build/interbotix_common_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ws/interbotix/build/interbotix_common_toolbox/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/derek-dietz/MSAI/ComputerVision/Final_Project/ws/interbotix/install/share/interbotix_common_modules/package.sh"
-. "/home/derek-dietz/MSAI/ComputerVision/Final_Project/ws/interbotix/install/share/interbotix_common_sim/package.sh"
